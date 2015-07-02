315197e20690b627b989ea65f045f32cdcee1f85
Mina Shnoudah <mina@shnoudah.com>
2015-07-02 16:25:08 -0400

    added static to petfind

--- petsite/settings.py
+++ petsite/settings.py
@@ -103,9 +103,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+'''
 STATICFILES_DIRS = (
     os.path.join(
         os.path.dirname(__file__),
         '../../static',
     ),
-)
+)'''
